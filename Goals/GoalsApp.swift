a29e9245d36987e4f421df483c896aef66ce0bcf
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2023-11-19 21:31:31 +0200

    prepare the app

---
+++ Goals/GoalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsApp.swift
+//  Goals
+//
+//  Created by Abanoub Ashraf on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
